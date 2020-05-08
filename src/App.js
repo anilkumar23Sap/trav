a1aa797b0af9ec193a52a93e4f1813ff50945a59
Anil Kumar <anikumar23@publicisgroupe.net>
2020-05-08 13:38:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import AppRouting from "./AppRouting";
+import "./App.css";
+class componentName extends Component {
+  render() {
+    return (
+      <main>
+        <AppRouting />
+      </main>
+    );
+  }
+}
+
+export default componentName;
